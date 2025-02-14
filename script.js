2578ca00933fef01cbf761cefaa34c5bccb65b63
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-14 10:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+  let val = document.getElementByID("sample Table");
+
+	let nr = table.insertRow(0);
+	let c1 = nr.insertCell(0);
+	let c2 = nr.insertCell(1);
+	
+	 c1.innerHtml = "New Cell1";
+	 c2.innerHtml = "New Cell2"
   
 }
